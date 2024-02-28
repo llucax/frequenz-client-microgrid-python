b852302bcdf867a0a197d0609fbf02712debdec9
Leandro Lucarella <luca-frequenz@llucax.com>
2024-02-28 14:05:49 +0100

    Rename module `metadata` to `_metadata`
    All the other modules are private, so there is no reason why this
    shouldn't be either.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- src/frequenz/client/microgrid/_client.py
+++ src/frequenz/client/microgrid/_client.py
@@ -48,8 +48,8 @@ from ._component_data import (
 )
 from ._connection import Connection
 from ._constants import RECEIVER_MAX_SIZE
+from ._metadata import Location, Metadata
 from ._retry import LinearBackoff, RetryStrategy
-from .metadata import Location, Metadata
 
 DEFAULT_GRPC_CALL_TIMEOUT = 60.0
 """The default timeout for gRPC calls made by this client (in seconds)."""
